4a7ce32fa093bea726e52c9c47ce49eed573349c
karm1000 <karmdsoni8159@gmail.com>
2024-03-02 01:11:06 +0530

    make product card

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,13 +1,44 @@
 import React from "react";
-import NavBar from "./NavBar";
 import "./Dashboard.css";
 import Filter from "./Sort";
+import ProductCard from "../ProductCard/ProductCard";
 
 function Dashboard() {
+  let product = {
+    "name": "Classic Wooden Chess Set",
+    "brand": "GrandMaster",
+    "images": [
+      {
+        "img": "https://m.media-amazon.com/images/I/519k3bRnnFL._SL1080_.jpg",
+        "contentType": "image/jpeg"
+      }
+    ],
+    "description": "Handcrafted wooden chess set with beautifully carved pieces, perfect for players of all skill levels.",
+    "category": ["Toys & Games", "Board Games"],
+    "price": 39.99,
+    "averageStar": 4.4,
+    "remarks": {
+      "colors": [
+        {"color": "Natural Wood"}
+      ],
+      "sizes": [
+        {"size": "Standard"}
+      ]
+    },
+    discount:50,
+
+  }
   return (
     <>
       <div>
         <Filter />
+      </div>
+      <div style={{
+        display:"flex",
+        flexWrap:"wrap",
+        flexDirection:"row"
+      }}>
+        <ProductCard product={product}/>
       </div>
     </>
   );
---
+++ src/components/ProductCard/ProductCard.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import styles from './ProductCard.module.css'
+import { Link } from 'react-router-dom';
+
+const ProductCard = ({product}) => {
+  let discountedPrice = parseFloat(product.price*product.discount/100).toPrecision(4);
+  return (
+    <div className={styles.product_card}>
+      <div className={styles.image}>
+        <img src={product.images[0].img} alt={product.name}/>
+      </div>
+      <div className={styles.info}>
+        <Link src={"/"} className={styles.name}>
+          {product.name}
+        </Link>
+        <div className={styles.brand}>
+          {product.brand}
+        </div>
+        <div className={styles.averageStar}>Average Rating: {product.averageStar}/5</div>
+        <div className={styles.pricing}>
+          <div className={styles.discounted}>
+            <span>M.R.P:</span>
+            <span className={styles.price}>{discountedPrice}</span>
+          </div>
+          <div className={styles.original}>
+            <span className={styles.price}>{product.price}</span>
+          </div>
+          <div className={styles.discount}>
+            ( {product.discount}% off ) 
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default ProductCard
