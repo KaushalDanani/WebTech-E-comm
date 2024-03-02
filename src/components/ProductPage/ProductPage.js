80a9ba3a1edc9a01870d67796b01fe491f6903b5
Kaushal <kdanani209@gmail.com>
2024-03-02 14:39:27 +0530

    ProductPage Update 2

--- src/components/ProductPage/ProductPage.js
+++ src/components/ProductPage/ProductPage.js
@@ -1,21 +1,26 @@
-import React, { useEffect, useRef } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import "./ProductPage.css";
+
+function ColorStyle(props) {
+  return (
+    <>
+      <input type='radio' value={props.type} radioGroup='type' id={props.type} checked={props.selectedOption === props.type} onChange={props.handleChange} />
+      <label className='colorsTags' id={props.type} htmlFor={props.type} style={{backgroundColor: props.type}}></label>
+    </>
+  )
+}
 
 function ProductPage() {
 
   const dprice = useRef();
+  const [selectedOption, setSelectedOption] = useState();
+  const category = ["Toys & Games", "Board Games", "New Product" , "What is this?", "Yahh Great"]
+  const colors = ["green", "yellow", "Black", "Orange"]
 
-  // function discountPrice() {
-  //   // const dp = await ((props.discount*props.price)/100);
-  //   let dp = (64*8999)/100;
-  //   dprice.current = dp;
-  //   console.log("Gh : "+dprice.current);
-  // }
+  function handleChange(event) {
+    setSelectedOption(event.target.value);
+  }
 
-  // useEffect(() => {
-    
-  //   discountPrice()
-  // }, [])
   
 
   return (
@@ -27,18 +32,22 @@ function ProductPage() {
             </div>
             <div className='ProductDetails'>
                 <span className='pname'> <strong>FZ Forza Brace Lucio Badminton Shoes </strong> </span>
+                <span className='multiCategory'> {category.map((type) => (<span className='categoryTags'>{type}</span>))} </span>
                 <span>Ratings : <span> <strong> {/* Enter Star rating */} </strong> </span> out of 5 star</span> 
                 <hr className='hrLine'/>
                 <span className='discounttag'> -64% <span className='priceTag'>₹4599 </span> </span>
                 <span className='productMRP'>M.R.P.: <s>₹8999</s></span>
                 <span> Inclusive of all taxes | <span><strong> No of Order: 1208 </strong></span></span>
+                <span className='colorHeading'><strong> Color: </strong></span>
+                <span className='multiColor'> {colors.map((type) => (<ColorStyle type={type} selectedOption={selectedOption} handleChange={handleChange}/>))} </span>
                 <div> <button className='addToCartBtn'><strong>Add to Cart</strong></button></div>
                 <span> <button className='buyNowBtn'><strong>Buy Now</strong></button></span>
-
-                <span className='dscriptionHeader'> <strong>Product Discription:</strong> </span>
-                <span className='productBand'> <strong>Brand:</strong> <span>Bata</span> </span>
-                <span className='productDiscription'> Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore hic eum impedit quod expedita possimus placeat magni eos repellendus qui, illum itaque aut nihil in temporibus atque cupiditate dicta aperiam doloribus, et perspiciatis commodi esse! Deleniti illum repellendus corrupti perspiciatis saepe maiores modi, cupiditate ullam porro amet laudantium, velit architecto explicabo ex. Ratione aut rerum, saepe numquam consectetur iusto vel, quaerat suscipit pariatur earum sit ex ipsum quae. Obcaecati, reiciendis praesentium? Dicta optio qui ea doloremque consequatur corrupti molestiae ducimus? </span>
+                
             </div>
+            <div className='ProductDetailDiscription'>
+              <span className='dscriptionHeader'> <strong>Product Discription:</strong> </span>
+                <span className='productBand'> <strong>Brand:</strong> <span>Bata</span> </span>
+                <span className='productDiscription'> Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore hic eum impedit quod expedita possimus placeat magni eos repellendus qui, illum itaque aut nihil in temporibus atque cupiditate dicta aperiam doloribus, et perspiciatis commodi esse! Deleniti illum repellendus corrupti perspiciatis saepe maiores modi, cupiditate ullam porro amet laudantium, velit architecto explicabo ex. Ratione aut rerum, saepe numquam consectetur iusto vel, quaerat suscipit pariatur earum sit ex ipsum quae. Obcaecati, reiciendis praesentium? Dicta optio qui ea doloremque consequatur corrupti molestiae ducimus? </span></div>
         </div>
     </div>
   )
